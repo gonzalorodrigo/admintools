1ac97b6ccbee1a9ac96aac9ec16796102ef474c5
Gonzalo Pedro Rodrigo Alvarez (ACS Student -LR) <gonzalo@corona.lbl.gov>
2017-10-26 01:21:54 -0700

    function to search an LBA in partitions

--- disk/fs_check.sh
+++ disk/fs_check.sh
@@ -13,12 +13,18 @@ function get_partitions_disk() {
 function search_partition() {
   disk=$1
   lba=$2
-  partition_list=( $( get_partitions_disk "$1" ) )
+  partition_list=( $( get_partitions_disk "$disk" ) )
   length=${#partition_list[@]}
   for ((i=0;i<$length;i+=3))
   do
-    echo ${partition_list[$i]}
-
+    partition=${partition_list[$i]}
+    start_lba=${partition_list[($i+1)]}
+    end_lba=${partition_list[($i+2)]}
+    if [ $lba -ge $start_lba ] && [ $lba -le $end_lba ] 
+    then
+      echo "$partition"
+      break
+    fi
   done
 }
 
@@ -28,7 +34,10 @@ function check_disk_ok() {
   if [ ! -z "$failure_lba" ]
   then
     echo "Disk($1) contains block failures at LBA $failure_lba"
+    broken_partition=( $( search_partition $1 $failure_lba ) )
+    echo "Failed partition is $broken_partition"
   fi  
+
 }
 
 
@@ -39,5 +48,5 @@ for disk_name in ${disks[@]}
 do
   echo "Testing disk $disk_name"
   check_disk_ok $disk_name
-  search_partition $disk_name
+  search_partition $disk_name 184131582
 done
